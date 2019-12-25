94146c4c6c71d652c48a5415a00d5aaa5e6b99eb
swym <0xfd000000@gmail.com>
2019-12-25 22:48:52 +0100

    add GPL v3 license file

--- lua_make.c
+++ lua_make.c
@@ -1,19 +1,15 @@
-/* Minimal Lua embedding into GNU Makefiles via GNU make's
-  loadable objects feature (introduced in make 4.0)
-
-Copyright (C) 2019 swym (0xfd000000@gmail.com)
-
-GNU Make is free software; you can redistribute it and/or modify it under the
-terms of the GNU General Public License as published by the Free Software
-Foundation; either version 3 of the License, or (at your option) any later
-version.
-
-GNU Make is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-this program.  If not, see <http://www.gnu.org/licenses/>.  */
+/**
+ * Minimal Lua embedding into GNU Makefiles via GNU make's
+ * loadable objects feature (introduced in make 4.0)
+ * 
+ * Copyright (C) 2019 swym (0xfd000000@gmail.com)
+ * 
+ * 
+ * This software is released under the terms of
+ *     version 3 of the GNU General Public License.
+ * 
+ * see the file LICENSE for the full licene
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
